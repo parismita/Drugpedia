42fc7d8832463f0de9c90f6c6a358669c20c97dc
parismita <das.parismita@gmail.com>
2022-09-23 00:37:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+import setuptools
 
+setuptools.setup(
+name="CS 699 Project",
+version="1.0.0",
+author="Parismita Das, Vaibhav Singh",
+author_email="das.parismita@gmail.com",
+url="<https://github.com/parismita/CS699",
+packages=setuptools.find_packages(),
+classifiers=[ "Programming Language :: Python :: 3",
+             "License :: OSI Approved :: MIT License",
+             "Operating System :: OS Independent"],
+    python_requires='>=3.4',
+)
