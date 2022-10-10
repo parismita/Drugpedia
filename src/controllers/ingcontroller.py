0d291d5862524a32e116b44c949c9ba36037c0de
Vaibhav Singh <53625245+VaibhavSingh29@users.noreply.github.com>
2022-10-10 23:48:44 +0530

    bs4 scraping for ingredients (#23)
    Co-authored-by: Vaibhav Singh <contacttovaibh@v@gmail.com>

---
+++ src/controllers/ingcontroller.py
@@ -0,0 +1,20 @@
+import json
+from src.models.ingredient import Ingredients
+from src.utils.initdb import db, create_db
+from src.services.ingservice import IngredientSearch
+
+def index():
+    return {
+        'status': 'OK',
+        'code': 200,
+        'data': ''
+    }
+
+
+def create():
+    create_db()
+
+
+# insert data into table.
+def search(): 
+    return IngredientSearch()    
--- src/services/ingservice.py
+++ src/services/ingservice.py
@@ -1,2 +1,29 @@
 #https://www.webmd.com/drugs/2/search?type=drugs&query=key
 #301-http status - moved perm means dont show these in search result
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def IngredientSearch(key):
+    search_url = "https://www.webmd.com/drugs/2/drug-17633/pantoprazole-oral/details"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+    source = requests.get(search_url, headers = headers)
+    soup = bs(source.content)
+
+    drug_header = soup.find("h1", attrs = {"class": "drug-name"})
+    drug_name = drug_header.get_text()
+    print(drug_name)
+
+    sections = ['uses-container', 'side-effects-container', 'precautions-container']
+
+    for section in sections:
+        container = soup.find("div", attrs = {"class": section})
+        monograph_content_headline = container.find("div", attrs = {"class": "title-bg"})
+        monograph_content = container.find("div", attrs = {"class": "monograph-content"})
+        title = monograph_content_headline.get_text(strip = True)
+        description = monograph_content.get_text()
+        print(title)
+        print(description)
+    
+    
