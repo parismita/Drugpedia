19e0daa1f6121aff78504283e6aa471406ff94e5
Vaibhav Singh <53625245+VaibhavSingh29@users.noreply.github.com>
2022-11-21 21:07:22 +0530

    Vaibhav/feature/integratingMedicineSearch (#31)
    Co-authored-by: Vaibhav Singh <contacttovaibh@v@gmail.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_migrate import Migrate
+from flask_session import Session
 from src.utils.initdb import db
 
 # routes here
@@ -36,7 +37,9 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://" + uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
     db.init_app(app)
     return app
 
