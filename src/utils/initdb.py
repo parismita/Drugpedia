9f57b3730b5b2a74642e6fb4f97717149b3ecb62
parismita <parismita@esmagico.in>
2022-11-22 23:06:46 +0530

    db create rewrite

--- src/utils/initdb.py
+++ src/utils/initdb.py
@@ -2,14 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 def create_db():
     try:
         # create tables if not exists.
         db.create_all()
         db.session.commit()
-        return '==================TABLES CREATED=================='
+        return
 
     except Exception as e:
         print(e)
-        return '==================TABLES NOT CREATED!!!=================='
+        return
