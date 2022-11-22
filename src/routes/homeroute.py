54d1fe0eb66a1ac34cc8e721609b23cf2ed6ad96
parismita <parismita@esmagico.in>
2022-11-22 23:07:20 +0530

    fix

--- src/routes/homeroute.py
+++ src/routes/homeroute.py
@@ -4,4 +4,4 @@ from src.controllers.homecontroller import index, create
 home = Blueprint('', __name__)
 
 home.route("/", methods=['POST', 'GET'])(index)
-home.route("/", methods=['POST', 'GET'])(create)
+home.route("/create", methods=['POST', 'GET'])(create)
